0154d7856953f5108756efc5c5f7ae6dd7b69727
TalhaCh-Coder <talhaahmeduni022@gmail.com>
2025-11-05 14:47:09 +0500

    About page is created

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
-import Hero from '../component/about/Hero'
+import AboutHero from '../component/about/AboutHero'
+import AboutStory from '../component/about/AboutStory'
+import AboutValues from '../component/about/AboutValues'
+import AboutPackaging from '../component/about/AboutPackaging'
+import AboutTeam from '../component/about/AboutTeam'
+import AboutCTA from '../component/about/AboutCTA'
+
 
 const About = () => {
   return (
-    <div>
-      <Hero/>
+    <div className="overflow-hidden">
+      <AboutHero />
+      <AboutStory />
+      <AboutValues />
+      <AboutPackaging />
+      <AboutTeam />
+      <AboutCTA />
     </div>
   )
 }
