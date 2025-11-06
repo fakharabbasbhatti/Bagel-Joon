844f9eabf5b8eea0cac6a193f69828e79a44b849
TalhaCh-Coder <talhaahmeduni022@gmail.com>
2025-11-06 14:30:17 +0500

    vercel ser up

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Hero from '../component/shop/Hero'
-import Categories from '../component/shop/categories'
+import Categories from "../component/shop/categories.jsx";
 import FeaturedProducts from '../component/shop/FeaturedProducts'
 import Sustainability from '../component/shop/Sustainability'
 import LifestyleGallery from '../component/shop/LifestyleGallery'
